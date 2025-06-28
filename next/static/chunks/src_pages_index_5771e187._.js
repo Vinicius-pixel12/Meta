74f55ef356025684cf905495221b7ccee8463361
Vinicius-pixel12 <vinicosta0@gmail.com>
2025-06-28 01:31:47 -0300

    Create src_pages_index_5771e187._.js

---
+++ next/static/chunks/src_pages_index_5771e187._.js
@@ -0,0 +1,11 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/node_modules_next_dist_eb81a647._.js",
+  "static/chunks/node_modules_react_1cad9b0b._.js",
+  "static/chunks/node_modules_react-dom_cjs_react-dom_development_ab7e073c.js",
+  "static/chunks/node_modules_react-dom_f14d0471._.js",
+  "static/chunks/node_modules_69e050bc._.js",
+  "static/chunks/[root of the server]__659f318b._.js",
+  "static/chunks/src_styles_globals_4738091e.css",
+  "static/chunks/src_pages__app_5771e187._.js",
+  "static/chunks/src_pages__app_52220960._.js"
+])
