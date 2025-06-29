d463711aa8bd0442ba3012d20b597eef16d34b39
Vinicius-pixel12 <vinicosta0@gmail.com>
2025-06-29 08:36:37 -0300

    Update transform.js

--- next/transform.js
+++ next/transform.js
@@ -1,11 +1,7 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/node_modules_next_dist_eb81a647._.js",
-  "static/chunks/node_modules_react_1cad9b0b._.js",
-  "static/chunks/node_modules_react-dom_cjs_react-dom_development_ab7e073c.js",
-  "static/chunks/node_modules_react-dom_f14d0471._.js",
-  "static/chunks/node_modules_69e050bc._.js",
-  "static/chunks/[root of the server]__659f318b._.js",
-  "static/chunks/src_styles_globals_4738091e.css",
-  "static/chunks/src_pages__app_5771e187._.js",
-  "static/chunks/src_pages__app_52220960._.js"
-])
+const CHUNK_PUBLIC_PATH = "transform.js";
+const runtime = require("./build/chunks/[turbopack]_runtime.js");
+runtime.loadChunk("build/chunks/postcss_config_mjs_transform_ts_f0ffbaad._.js");
+runtime.loadChunk("build/chunks/[root of the server]__04d7a048._.js");
+runtime.getOrInstantiateRuntimeModule("[turbopack-node]/globals.ts [postcss] (ecmascript)", CHUNK_PUBLIC_PATH);
+runtime.getOrInstantiateRuntimeModule("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[project]/postcss.config.mjs/transform.ts { CONFIG => \\\"[project]/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)", CHUNK_PUBLIC_PATH);
+module.exports = runtime.getOrInstantiateRuntimeModule("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[project]/postcss.config.mjs/transform.ts { CONFIG => \\\"[project]/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
