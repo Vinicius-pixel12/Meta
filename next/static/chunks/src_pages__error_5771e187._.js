e1852d8c8f6a1f3b7646e469d7ba5695b66645ce
Vinicius-pixel12 <vinicosta0@gmail.com>
2025-06-29 00:58:34 -0300

    Update src_pages__error_5771e187._.js

--- next/static/chunks/src_pages__error_5771e187._.js
+++ next/static/chunks/src_pages__error_5771e187._.js
@@ -1,11 +1,16 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/node_modules_next_dist_eb81a647._.js",
-  "static/chunks/node_modules_react_1cad9b0b._.js",
-  "static/chunks/node_modules_react-dom_cjs_react-dom_development_ab7e073c.js",
-  "static/chunks/node_modules_react-dom_f14d0471._.js",
-  "static/chunks/node_modules_69e050bc._.js",
-  "static/chunks/[root of the server]__659f318b._.js",
-  "static/chunks/src_styles_globals_4738091e.css",
-  "static/chunks/src_pages__app_5771e187._.js",
-  "static/chunks/src_pages__app_52220960._.js"
-])
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_pages__error_5771e187._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_pages__error_5771e187._.js",
+  "chunks": [
+    "static/chunks/[root of the server]__73499ecc._.js",
+    "static/chunks/node_modules_react_b2385d85._.js",
+    "static/chunks/node_modules_react-dom_cjs_react-dom_development_ab7e073c.js",
+    "static/chunks/node_modules_react-dom_f14d0471._.js",
+    "static/chunks/node_modules_69e050bc._.js",
+    "static/chunks/[root of the server]__ca38f087._.js"
+  ],
+  "source": "entry"
+});
