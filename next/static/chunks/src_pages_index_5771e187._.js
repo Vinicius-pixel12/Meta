6e4abb46f5d7871b09d756d2f090b8a25fba954e
Vinicius-pixel12 <vinicosta0@gmail.com>
2025-06-29 08:29:34 -0300

    Update src_pages_index_5771e187._.js

--- next/static/chunks/src_pages_index_5771e187._.js
+++ next/static/chunks/src_pages_index_5771e187._.js
@@ -1,11 +1,20 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/node_modules_next_dist_eb81a647._.js",
-  "static/chunks/node_modules_react_1cad9b0b._.js",
-  "static/chunks/node_modules_react-dom_cjs_react-dom_development_ab7e073c.js",
-  "static/chunks/node_modules_react-dom_f14d0471._.js",
-  "static/chunks/node_modules_69e050bc._.js",
-  "static/chunks/[root of the server]__659f318b._.js",
-  "static/chunks/src_styles_globals_4738091e.css",
-  "static/chunks/src_pages__app_5771e187._.js",
-  "static/chunks/src_pages__app_52220960._.js"
-])
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_pages_index_5771e187._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_pages_index_5771e187._.js",
+  "chunks": [
+    "static/chunks/[root of the server]__b1cae77f._.js",
+    "static/chunks/node_modules_next_49c53b91._.js",
+    "static/chunks/node_modules_react_1cad9b0b._.js",
+    "static/chunks/node_modules_react-dom_cjs_react-dom_development_ab7e073c.js",
+    "static/chunks/node_modules_react-dom_f14d0471._.js",
+    "static/chunks/node_modules_lodash_fe323c00._.js",
+    "static/chunks/node_modules_recharts_es6_3e272a6f._.js",
+    "static/chunks/node_modules_@radix-ui_a0801a3b._.js",
+    "static/chunks/node_modules_@floating-ui_056e3f9a._.js",
+    "static/chunks/node_modules_04eb5efe._.js"
+  ],
+  "source": "entry"
+});
